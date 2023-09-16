3adfa363eb3a6e3a1d10cf0b29f57e01048b04c7
Marcelo Vieira <marcelodefy15@gmail.com>
2023-09-16 11:52:12 -0300

    Asynchronicity

--- Program.cs
+++ Program.cs
@@ -7,44 +7,42 @@ namespace BIManager
 {
     public static class Program
     {
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            var usuario = new Usuario
-            {
-                NomeUsuario = "MArcelo",
-                Email = "contato2@email.com",
-                Senha = "1233",
-                BasesDeDados = new List<BaseDeDados> { new BaseDeDados {
-                    Nome = "Base 1",
-                    UrlConexao = "urldeexemplo2.com",
-                    }},
-                SenhaAleatoria = true,
-            };
+            // var usuario = new Usuario
+            // {
+            //     NomeUsuario = "MArcelo2",
+            //     Email = "contato22@email.com",
+            //     Senha = "12332",
+            //     BasesDeDados = new List<BaseDeDados> { new BaseDeDados {
+            //         Nome = "Base 1",
+            //         UrlConexao = "urldeexemplo222.com",
+            //         }},
+            //     SenhaAleatoria = true,
+            // };
 
-            using (var ctx = new BIManagerDataContext())
-            {
-                var role = ctx.Funcao.FirstOrDefault(x => x.Nome == "User");
-                if (role == null)
-                    return;
+            using var ctx = new BIManagerDataContext();
+            // var role = await ctx.Funcao.FirstOrDefaultAsync(x => x.Nome == "User");
+            // if (role == null)
+            //     return;
 
-                usuario.Funcao = role;
-                ctx.Usuarios.Add(usuario);
-                ctx.SaveChanges();
+            // usuario.Funcao = role;
+            // ctx.Usuarios.Add(usuario);
+            // await ctx.SaveChangesAsync();
 
-                var usuarios = ctx.Usuarios.Include(x => x.BasesDeDados).Include(x => x.Funcao).AsNoTracking().ToList();
-                var funcoes = ctx.Funcao.ToList();
-                var bases = ctx.BasesDeDados.ToList();
+            var usuarios = await ctx.Usuarios.Include(x => x.BasesDeDados).Include(x => x.Funcao).AsNoTracking().ToListAsync();
+            var funcoes = await ctx.Funcao.AsNoTracking().ToListAsync();
+            var bases = await ctx.BasesDeDados.AsNoTracking().ToListAsync();
 
 
-                foreach (var user in usuarios)
-                    Console.WriteLine($"{user.NomeUsuario} - {user.Email} - {user.Funcao?.Nome}");
+            foreach (var user in usuarios)
+                Console.WriteLine($"{user.NomeUsuario} - {user.Email} - {user.Funcao?.Nome}");
 
-                foreach (var funcao in funcoes)
-                    Console.WriteLine(funcao.Nome);
+            foreach (var funcao in funcoes)
+                Console.WriteLine(funcao.Nome);
 
-                foreach (var baseDeDados in bases)
-                    Console.WriteLine(baseDeDados.Nome);
-            }
+            foreach (var baseDeDados in bases)
+                Console.WriteLine(baseDeDados.Nome);
 
 
 
